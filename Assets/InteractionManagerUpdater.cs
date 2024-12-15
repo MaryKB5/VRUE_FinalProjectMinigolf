6f697871d09644ec63b9a37ebfe8ee0f94ad030c
Michael Auß <michael.auss@posteo.at>
2024-12-15 23:59:04 +0100

    Fix multiplayer interaction

--- Assets/InteractionManagerUpdater.cs
+++ Assets/InteractionManagerUpdater.cs
@@ -8,29 +8,47 @@ public class InteractionManagerUpdater : MonoBehaviour
     private TeleportationAnchor teleportationAnchor;
     private TeleportationArea teleportationArea;
 
+    private XRGrabInteractable xRGrabInteractable;
+
     // Start is called before the first frame update
     void Start()
     {
         teleportationAnchor = GetComponent<TeleportationAnchor>();      
         teleportationArea = GetComponent<TeleportationArea>();
+        xRGrabInteractable = GetComponent<XRGrabInteractable>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-        if (teleportationAnchor != null && teleportationAnchor.interactionManager == null) {    
-            Debug.Log("TeleportationAnchor interactionmanager is null");
-            XRInteractionManager xRInteractionManager = GameObject.FindAnyObjectByType<XRInteractionManager>();
-            Debug.Log("Updating TeleportationAnchor.interactionManager to xrInteractionManager: " + xRInteractionManager);
-            teleportationAnchor.interactionManager = xRInteractionManager;
-        }
+        private float nextActionTime = 0.0f;
+    private float period = 1.0f;
 
-        if (teleportationArea != null && teleportationArea.interactionManager == null) {    
-            Debug.Log("TeleportationArea interactionmanager is null");
-            XRInteractionManager xRInteractionManager = GameObject.FindAnyObjectByType<XRInteractionManager>();
-            Debug.Log("Updating TeleportationArea.interactionManager to xrInteractionManager: " + xRInteractionManager);
-            teleportationArea.interactionManager = xRInteractionManager;
+
+
+    void Update() {
+        if (Time.time > nextActionTime ) {
+            nextActionTime += period;
+        
+            if (teleportationAnchor != null ) { 
+                Debug.Log("TeleportationAnchor interactionmanager is null");
+                XRInteractionManager xRInteractionManager = GameObject.FindAnyObjectByType<XRInteractionManager>();
+                Debug.Log("Updating TeleportationAnchor.interactionManager to xrInteractionManager: " + xRInteractionManager);
+                teleportationAnchor.interactionManager = xRInteractionManager;
+            }
+
+            if (teleportationArea != null ) { 
+                Debug.Log("TeleportationArea interactionmanager is null");
+                XRInteractionManager xRInteractionManager = GameObject.FindAnyObjectByType<XRInteractionManager>();
+                Debug.Log("Updating TeleportationArea.interactionManager to xrInteractionManager: " + xRInteractionManager);
+                teleportationArea.interactionManager = xRInteractionManager;
+            }
+
+
+            if (xRGrabInteractable != null ) {    
+                Debug.Log("xRGrabInteractable interactionmanager is null");
+                XRInteractionManager xRInteractionManager = GameObject.FindAnyObjectByType<XRInteractionManager>();
+                Debug.Log("Updating xRGrabInteractable.interactionManager to xrInteractionManager: " + xRInteractionManager);
+                xRGrabInteractable.interactionManager = xRInteractionManager;
+            }
         }
 
     }
